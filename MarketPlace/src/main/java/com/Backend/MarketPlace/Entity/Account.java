843dc7f7b9e4f487d8033f88c1d25434f4a71c0c
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-08-23 19:50:01 +0530

    Backed Connected

---
+++ MarketPlace/src/main/java/com/Backend/MarketPlace/Entity/Account.java
@@ -0,0 +1,26 @@
+package com.Backend.MarketPlace.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "Customers")
+public class Account {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+private Long accountId;
+    @Column(name = "Name")
+private String accountName;
+    @Column(name = "EmailAddress")
+private String email;
+    @Column(name = "MobileNumber",nullable = false , unique = true)
+private String mobileNumber;
+
+}
