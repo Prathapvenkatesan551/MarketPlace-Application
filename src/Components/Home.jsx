6fe14b21fbd530b0bf036c0200fb037460c8702f
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-08-25 03:00:17 +0530

    renewed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './Home.css';
-import MarketCards from './MarketCards';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import MarketCards, { NewProducts, RenewedProducts } from "./MarketCards";
 
-const images = [
-  'Diwali.jpg',
-  'Blacksale.jpg',
-  'Banner-1.jpg'
-];
+const images = ["Diwali.jpg", "Blacksale.jpg", "Banner-1.jpg"];
 
 const Home = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -14,34 +10,32 @@ const Home = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 15000); 
+    }, 15000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className='home'>
-      <img src={images[currentImage]} alt={`Slide ${currentImage}`} className='banner-1' />
-      <div className='scaleCont'>
+    <div className="home">
+      <img
+        src={images[currentImage]}
+        alt={`Slide ${currentImage}`}
+        className="banner-1"
+      />
+      <div className="scaleCont">
         <h1>Buy Your New Products Here !</h1>
-        <div className='scaleItems'>
-      <MarketCards/>
-      <MarketCards/>
-      <MarketCards/>
-      <MarketCards/>
-      </div>
+        <div className="scaleItems">
+          <NewProducts />
         </div>
-        <div className='scaleCont'>
+      </div>
+      <div className="scaleCont">
         <h1>Buy Your Renewed Products Here !</h1>
-        <div className='scaleItems'>
-      <MarketCards/>
-      <MarketCards/>
-      <MarketCards/>
-      <MarketCards/>
-      </div>
+        <div className="scaleItems">
+          <RenewedProducts/>
         </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
--- src/Components/MarketCards.jsx
+++ src/Components/MarketCards.jsx
@@ -1,13 +1,85 @@
 import React from 'react'
 import './MarketCards.css'
-const MarketCards = () => {
+import PropTypes from 'prop-types'
+const MarketCards = (props) => {
   return (
     <div className='marketCard'>
-      <img src="car.jpeg" alt=""  className='car'/>
-      <label htmlFor="" className='carLable'>Cars for scale</label>
-      <label htmlFor="" className='offerLabel'>20% offer for new buyer</label>
+      <img src={props.imageSrc} alt=""  className='car'/>
+      <label htmlFor="" className='carLable'>{props.label_1}</label>
+      <label htmlFor="" className='offerLabel'>{props.label_2}</label>
     </div>
   )
 }
-
+export const NewProducts=()=>{
+    let newProducts=[
+        {
+            label_1:"Mobiles for scale",
+            label_2:"20% offer for new buyer",
+            imageSrc:"mobile.png"
+        },
+        {
+            label_1:"Laptop for scale",
+            label_2:"10% offer for new buyer",
+            imageSrc:"laptop.jpg"
+        },
+        {
+            label_1:"Books for scale",
+            label_2:"50% offer for new buyer",
+            imageSrc:"books.jpg"
+        },
+        {
+            label_1:"Furnitures for scale",
+            label_2:"20% offer for new buyer",
+            imageSrc:"furnitures.jpg"
+        }
+    ];
+    return(
+        <div className="newProducts"> 
+            {newProducts.map((product, index) => (
+                <MarketCards
+                    key={index}
+                    label_1={product.label_1}
+                    label_2={product.label_2}
+                    imageSrc={product.imageSrc}
+                />
+            ))}
+        </div>
+    )
+}
+export const RenewedProducts=()=>{
+    let newProducts=[
+        {
+            label_1:"Renewed Mobiles for scale",
+            label_2:"Cost Reduced",
+            imageSrc:"usedMobiles.jpg"
+        },
+        {
+            label_1:"Renewed Laptop for scale",
+            label_2:"Cost Reduced",
+            imageSrc:"usedLaptops.jpg"
+        },
+        {
+            label_1:"Used Books for scale",
+            label_2:"Cost Reduced",
+            imageSrc:"usedBooks.jpg"
+        },
+        {
+            label_1:"Renewed Furnitures for scale",
+            label_2:"Cost Reduced",
+            imageSrc:"usedFurnitures.jpg"
+        }
+    ];
+    return(
+        <div className="newProducts"> 
+            {newProducts.map((product, index) => (
+                <MarketCards
+                    key={index}
+                    label_1={product.label_1}
+                    label_2={product.label_2}
+                    imageSrc={product.imageSrc}
+                />
+            ))}
+        </div>
+    )
+}
 export default MarketCards
