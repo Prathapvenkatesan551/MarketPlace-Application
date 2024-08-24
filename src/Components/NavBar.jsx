9930b2748fe02acd1501197a805200acf458693e
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-08-24 23:56:33 +0530

    transition

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import './NavBar.css'
-import { useState } from 'react';
-const NavBar = () => {
+import React, { useState } from 'react';
+import './NavBar.css';
 
+const NavBar = () => {
   const [showProfileMenu, setShowProfileMenu] = useState(false);
 
   const handleAccount = () => {
@@ -10,49 +9,57 @@ const NavBar = () => {
   };
 
   return (
-   <nav>
-      <div className='NavLeft'>
-       <label htmlFor="">MarketPlace</label>
-       <button>Explore </button>
+    <nav>
+      <div className="NavLeft">
+        <label>MarketPlace</label>
+        <button>Explore</button>
       </div>
-      <div className='NavRight'>
-        <button className='HC'>Home</button>
-        <button className='HC'>Contact us</button>
-        <button className='imageButton'>
-            <img src="cart.png" alt="" className='cartImage'/>
+      <div className="NavRight">
+        <button className="HC">Home</button>
+        <button className="HC">Contact us</button>
+        <button className="imageButton">
+          <img src="cart.png" alt="Cart" className="cartImage" />
         </button>
-        <button className='imageButton' id="Account" onClick={handleAccount}>
-         <img src="user.png" alt="" className='userImage' />
+        <button className="imageButton" id="Account" onClick={handleAccount}>
+          <img src="user.png" alt="User" className="userImage" />
         </button>
       </div>
-      {showProfileMenu && <ProfileMenu/>}
+      <div className={showProfileMenu ? 'profileMenu active' : 'profileMenu'}>
+        {showProfileMenu && <ProfileMenu />}
+      </div>
     </nav>
-  )
-}
-export const ProfileMenu=()=>{
-  return(
-    <div className='profileMenu'>
-     <div className='part-1'>
-      <label htmlFor="">Account</label>
-      <img src="user.png" alt="" className='profileImage'/>
-      <label htmlFor="">Prathap Venkatesan</label>
-      <label htmlFor="">prathap2venkatesan@gmail.com</label>
-     </div>
-     <div className='part-2'>
-      <button>
-      <img src="settings.png" alt="" className='Image' />
-        settings</button>
-      <button>
-      <img src="user.png" alt="" className='Image' />
-        Account</button>
-      <button>
-      <img src="high-score.png" alt="" className='Image' />
-        Market Score</button>
-      <button>
-      <img src="logout.png" alt="" className='Image' />
-        Log out</button>
-     </div>
+  );
+};
+
+export const ProfileMenu = () => {
+  return (
+    <div className="profileMenuContent">
+      <div className="part-1">
+        <label>Account</label>
+        <img src="user.png" alt="Profile" className="profileImage" />
+        <label>Prathap Venkatesan</label>
+        <label>prathap2venkatesan@gmail.com</label>
+      </div>
+      <div className="part-2">
+        <button>
+          <img src="settings.png" alt="Settings" className="Image" />
+          Settings
+        </button>
+        <button>
+          <img src="user.png" alt="Account" className="Image" />
+          Account
+        </button>
+        <button>
+          <img src="high-score.png" alt="Market Score" className="Image" />
+          Market Score
+        </button>
+        <button>
+          <img src="logout.png" alt="Log out" className="Image" />
+          Log out
+        </button>
+      </div>
     </div>
-  )
-}
-export default NavBar
+  );
+};
+
+export default NavBar;
