b942cbdb7621c37f85be98517ff4a7cbe9869763
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-08-27 00:26:18 +0530

    popup - view details

--- src/Components/NewMobile.jsx
+++ src/Components/NewMobile.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
+import React from 'react';
 import './NewMobile.css';
+import { useState } from 'react';
 
-
-export const MobileCard=()=>{
-  return(
+export const MobileCard = ({ onViewDetails }) => {
+  return (
     <div className='MCard'>
-      <img src="iphone.jpg" alt="Not yet Fixed" className='mobileImage'/>
+      <img src="iphone.jpg" alt="iPhone" className='mobileImage' />
       <div>
-       <h3>iphone-15 (Purple blue)</h3>
+        <h3>iPhone 15 (Purple Blue)</h3>
         <h4>15% offer - Rs 70,000</h4>
-        <p>128GB ram | 48 + 12 Mp |1 year warranty for In-box accessories </p>
-
+        <p>128GB RAM | 48 + 12 MP | 1 year warranty for in-box accessories</p>
       </div>
       <div className='cardButtons'>
         <button className='ATC'>Add to cart</button>
-        <button className='VD'>View Details</button>
+        <button className='VD' onClick={onViewDetails}>View Details</button>
       </div>
     </div>
-  )
-}
+  );
+};
 const NewMobile = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleViewDetails = () => {
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <div className='NewMobiles'>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
-      <MobileCard/>
+      {[...Array(16)].map((_, index) => (
+        <MobileCard key={index} onViewDetails={handleViewDetails} />
+      ))}
+      {isPopupOpen && (
+        <div className='popupOverlay'>
+          <div className='popupContent'>
+            <h2>iPhone 15 (Purple Blue)</h2>
+            <p>Detailed information about the iPhone 15...</p>
+            <button className='closeButton' onClick={handleClosePopup}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default NewMobile
+export default NewMobile;
