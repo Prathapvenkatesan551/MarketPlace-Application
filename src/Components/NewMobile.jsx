731981ca2f31b897fa41c7ca3916908d0a180ddb
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-08-28 16:03:37 +0530

    Popup button

--- src/Components/NewMobile.jsx
+++ src/Components/NewMobile.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './NewMobile.css';
-import { useState } from 'react';
+import React from "react";
+import "./NewMobile.css";
+import { useState } from "react";
 
 export const MobileCard = ({ onViewDetails }) => {
   return (
-    <div className='MCard'>
-      <img src="iphone.jpg" alt="iPhone" className='mobileImage' />
+    <div className="MCard">
+      <img src="iphone.jpg" alt="iPhone" className="mobileImage" />
       <div>
         <h3>iPhone 15 (Purple Blue)</h3>
         <h4>15% offer - Rs 70,000</h4>
         <p>128GB RAM | 48 + 12 MP | 1 year warranty for in-box accessories</p>
       </div>
-      <div className='cardButtons'>
-        <button className='ATC'>Add to cart</button>
-        <button className='VD' onClick={onViewDetails}>View Details</button>
+      <div className="cardButtons">
+        <button className="ATC">Add to cart</button>
+        <button className="VD" onClick={onViewDetails}>
+          View Details
+        </button>
       </div>
     </div>
   );
@@ -30,16 +32,19 @@ const NewMobile = () => {
   };
 
   return (
-    <div className='NewMobiles'>
+    <div className="NewMobiles">
       {[...Array(16)].map((_, index) => (
         <MobileCard key={index} onViewDetails={handleViewDetails} />
       ))}
       {isPopupOpen && (
-        <div className='popupOverlay'>
-          <div className='popupContent'>
+        <div className="popupOverlay">
+          <div className="popupContent">
             <h2>iPhone 15 (Purple Blue)</h2>
             <p>Detailed information about the iPhone 15...</p>
-            <button className='closeButton' onClick={handleClosePopup}>Close</button>
+            <button className="BookButton">Book now</button>
+            <button className="closeButton" onClick={handleClosePopup}>
+              Close
+            </button>
           </div>
         </div>
       )}
